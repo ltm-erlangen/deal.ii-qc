b256520158ee739636542a7459a6deb623d6b79d
vishalkenchan <vishalkenchan@users.noreply.github.com>
2017-04-28 20:12:30 +0200

    added PairCoulWolf and a test with blessed values from maxima file (#53)
    added PairCoulWolfManager and a test with blessed values from maxima file

---
+++ source/potentials/pair_coul_wolf.cc
@@ -0,0 +1,107 @@
+
+#include <cmath>
+
+#include <dealiiqc/potentials/pair_coul_wolf.h>
+
+namespace dealiiqc
+{
+  using namespace dealii;
+
+  namespace Potential
+  {
+
+    PairCoulWolfManager::PairCoulWolfManager ( const double &alpha,
+                                               const double &cutoff_radius,
+                                               const std::vector<types::charge> &charges)
+      :
+      alpha(alpha),
+      cutoff_radius(cutoff_radius),
+      charges(charges),
+      energy_shift(std::erfc(alpha *cutoff_radius)/cutoff_radius),
+      cutoff_radius_inverse(1./cutoff_radius),
+      compound_exp_value(std::exp(-alpha *alpha *cutoff_radius *cutoff_radius))
+    {}
+
+    void
+    PairCoulWolfManager::declare_interactions ( const types::atom_type i_atom_type,
+                                                const types::atom_type j_atom_type,
+                                                InteractionTypes interaction,
+                                                const std::vector<double> &parameters)
+    {
+      Assert( interaction==InteractionTypes::Coul_Wolf,
+              ExcMessage("Invalid InteractionTypes specified"));
+
+      Assert( i_atom_type < charges.size() && j_atom_type < charges.size(),
+              ExcMessage("Either the list of charges is initialized incorrectly"
+                         "Or atom type argument passed is incorrect"));
+
+      Assert( parameters.size() == 0,
+              ExcMessage("This class does not accept any parameters."));
+    }
+
+    template<bool ComputeScalarForce>
+    std::pair<double, double>
+    PairCoulWolfManager::energy_and_scalar_force ( const types::atom_type i_atom_type,
+                                                   const types::atom_type j_atom_type,
+                                                   const double &squared_distance) const
+    {
+      if ( squared_distance > cutoff_radius*cutoff_radius )
+        return ComputeScalarForce
+               ?
+               std::make_pair(0.,0.)
+               :
+               std::make_pair(0., std::numeric_limits<double>::signaling_NaN());
+
+      // TODO: Need to setup units
+      // The multiplying factor qqrd2e = 14.399645 yields energy in eV
+      // and force in eV/Angstrom units
+      const double qqrd2e = 14.399645;
+      const double distance = std::sqrt(squared_distance);
+
+      Assert( i_atom_type < charges.size() && i_atom_type < charges.size(),
+              ExcMessage("The function is called with a value of atom type "
+                         "larger than the size of PairCoulWolf::charges."
+                         "Please ensure that the PairCoulWolf::charges is "
+                         "initialized accurately."));
+
+      const double qiqj = (double) charges[i_atom_type] * charges[j_atom_type];
+      const double distance_inverse = 1.0/distance;
+      const double erfc_a_distance = std::erfc(alpha*distance) * distance_inverse;
+
+      const double energy = qiqj * ( erfc_a_distance - energy_shift ) * qqrd2e;
+
+      const double force = ComputeScalarForce
+                           ?
+                           qqrd2e * qiqj *
+                           ( distance_inverse *
+                             (
+                               erfc_a_distance + alpha*M_2_SQRTPI *
+                               std::exp(-alpha*alpha*squared_distance)
+                             )
+                             -
+                             cutoff_radius_inverse *
+                             (
+                               energy_shift + alpha * M_2_SQRTPI *
+                               compound_exp_value
+                             )
+                           )
+                           :
+                           std::numeric_limits<double>::signaling_NaN();
+
+      return std::make_pair(energy,force);
+    }
+
+    template
+    std::pair<double, double>
+    PairCoulWolfManager::energy_and_scalar_force <true> ( const types::atom_type i_atom_type,
+                                                          const types::atom_type j_atom_type,
+                                                          const double &squared_distance) const;
+
+    template
+    std::pair<double, double>
+    PairCoulWolfManager::energy_and_scalar_force <false> ( const types::atom_type i_atom_type,
+                                                           const types::atom_type j_atom_type,
+                                                           const double &squared_distance) const;
+  }
+
+}
---
+++ tests/potentials/pair_coul_wolf_01.cc
@@ -0,0 +1,63 @@
+
+#include <dealiiqc/potentials/pair_coul_wolf.h>
+#include <dealiiqc/utilities.h>
+
+// Short test to check validity of PairLJCut class energy_and_gradient
+// member function.
+// The blessed file is created using maxima script: pair_coul_wolf.mc file.
+
+using namespace dealiiqc;
+using namespace dealii;
+
+void test ( const double &r,
+            const double &alpha,
+            const double &cutoff_radius,
+            const double &blessed_energy,
+            const double &blessed_force)
+{
+  std::vector<dealiiqc::types::charge> charges = { 1., -1.};
+
+  Potential::PairCoulWolfManager coul_wolf ( alpha, cutoff_radius, charges);
+  coul_wolf.declare_interactions( 0,
+                                  1,
+                                  Potential::InteractionTypes::Coul_Wolf);
+
+  std::pair<double, double> energy_force_0 =
+    coul_wolf.energy_and_scalar_force( 0, 1, r*r);
+
+  std::cout << "Energy: " << energy_force_0.first << " "
+            << "Force scalar: " << energy_force_0.second << std::endl;
+
+  AssertThrow( fabs(energy_force_0.first-blessed_energy) < 100. * std::numeric_limits<double>::epsilon(),
+               ExcInternalError());
+  AssertThrow( fabs(energy_force_0.second-blessed_force) < 100. * std::numeric_limits<double>::epsilon(),
+               ExcInternalError());
+
+  std::pair<double, double> energy_force_1 =
+    coul_wolf.energy_and_scalar_force( 1, 0, r*r);
+
+  std::cout << "Energy: " << energy_force_1.first << " "
+            << "Force scalar: " << energy_force_1.second << std::endl;
+
+  AssertThrow( fabs(energy_force_1.first-blessed_energy) < 100. * std::numeric_limits<double>::epsilon(),
+               ExcInternalError());
+  AssertThrow( fabs(energy_force_1.second-blessed_force) < 100. * std::numeric_limits<double>::epsilon(),
+               ExcInternalError());
+
+  std::pair<double, double> energy_force_2 =
+    coul_wolf.energy_and_scalar_force<false>( 0, 1, r*r);
+
+  std::cout << "Energy: " << energy_force_2.first << " "
+            << "Force scalar: " << energy_force_2.second << std::endl;
+
+}
+
+int main()
+{
+
+  test(0.90, 0.25, 0.95, -0.8345031730789789, -1.829727499044526);
+  test(1.50, 0.25, 0.95,  0.,                  0.               );
+  test(1.00, 0.25, 1.75, -6.00939934656638,   -9.799065657452962);
+
+  return 0;
+}
