f6831df86e305de18c78df1ac8b9a5a8a773d3a9
vishalkenchan <vishal.boddu@fau.de>
2017-05-10 13:03:52 +0200

    updated documentation of AtomData and AtomHandler

--- include/dealiiqc/atom/atom_handler.h
+++ include/dealiiqc/atom/atom_handler.h
@@ -28,35 +28,38 @@ namespace dealiiqc
     AtomHandler( const ConfigureQC &configure_qc);
 
     /**
-     * Setup atom attributes namely:
-     * @see energy_atoms, @see masses and @see atomtype_to_atoms
+     * Setup cell based atom attributes in @p atom_data,  namely:
+     * AtomData::energy_atoms, AtomData::masses and AtomData::atomtype_to_atoms.
      *
      * For each and every atom in the system, find the locally relevant cell
      * of the @p mesh which surrounds it. If the atom doesn't belong to
      * any of the locally relevant cells, it is thrown. In the case when
      * a locally relevant cell is found, and if the atom is energy atom it is
-     * inserted into @see energy_atoms.
+     * inserted into AtomData::energy_atoms.
      *
      * A locally relevant cell is one which is either a locally owned cell or
-     * a ghost cell within a certain distance(determined by cluster radius and
-     * interaction radius). A ghost cell could contain atoms whose positions are
-     * relevant for computing energy or forces on locally relevant
+     * a ghost cell within a certain distance(determined by interaction radius
+     * between atoms in the system). A ghost cell could contain atoms whose
+     * positions are needed for computing energy or forces on locally owned
      * cluster atoms. An MPI process computes energy and forces only of it's
      * locally relevant cluster atoms.
      *
      * All the atoms which are not locally relevant energy_atoms
      * are thrown away. However, a count of the number of (locally relevant)
      * non-energy atoms (i.e., for which a locally relevant cell is found
-     * but are not energy atoms) is kept using @see n_thrown_atoms_per_cell
-     * for the sake of updating cluster weights.
+     * but are not energy atoms) is kept using AtomData::n_thrown_atoms_per_cell
+     * for the sake of updating cluster weights at a later stage.
      *
      */
     void parse_atoms_and_assign_to_cells( const types::MeshType<dim> &mesh,
                                           AtomData<dim> &atom_data) const;
 
     /**
-     * Return the neighbor lists of the @see energy_atoms.
-     * This function can be called as often as one deems necessary.
+     * Return the neighbor lists of AtomData::energy_atoms.
+     *
+     * This function can be called as often as one deems necessary. The function
+     * currently assumes that the deformation is small that the neighbor lists
+     * are exactly the same as that of reference (undeformed) configuration.
      */
     std::multimap< std::pair<types::ConstCellIteratorType<dim>, types::ConstCellIteratorType<dim>>, std::pair<types::CellAtomConstIteratorType<dim>, types::CellAtomConstIteratorType<dim> > >
         get_neighbor_lists( const types::CellAtomContainerType<dim> &energy_atoms) const;
