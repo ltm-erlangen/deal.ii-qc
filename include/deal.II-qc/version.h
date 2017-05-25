e24494ca5b29e608836639ecc94c75ffe7dabcbc
Denis Davydov <davydden@gmail.com>
2017-05-25 10:17:58 +0200

    query git info and store for output to terminal

---
+++ include/deal.II-qc/version.h
@@ -0,0 +1,14 @@
+#ifndef __dealii_qc_version_h
+#define __dealii_qc_version_h
+
+#include <string>
+
+namespace dealiiqc
+{
+  extern const std::string GIT_REVISION;
+  extern const std::string GIT_BRANCH;
+  extern const std::string GIT_SHORTREV;
+  extern const std::string GIT_TAG;
+}
+
+#endif
