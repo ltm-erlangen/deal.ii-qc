da6a14a2bbe9b22a623c9628954b715c3ad02c84
Vishal Boddu <vishal.boddu@fau.de>
2017-07-01 22:42:04 +0200

    fix qc documentation and minor variable name changes

--- include/deal.II-qc/core/qc.h
+++ include/deal.II-qc/core/qc.h
@@ -29,8 +29,6 @@ namespace LA
 
 #include <deal.II-qc/atom/molecule_handler.h>
 #include <deal.II-qc/configure/configure_qc.h>
-#include <deal.II-qc/potentials/pair_lj_cut.h>
-#include <deal.II-qc/potentials/pair_coul_wolf.h>
 
 
 DEAL_II_QC_NAMESPACE_OPEN
@@ -39,13 +37,17 @@ DEAL_II_QC_NAMESPACE_OPEN
 using namespace dealii;
 
 /**
- * Main class for the Quasi-continuum calculations
+ * A principal class for the fully non-local energy-based quasicontinuum
+ * calculations.
+ *
+ * @note QC only supports quasicontinuum description of a single Molecule
+ * type.
  */
 template <int dim, typename PotentialType>
 class QC
 {
 public:
-  QC ( const ConfigureQC &);
+  QC (const ConfigureQC &);
   ~QC ();
 
   void run ();
@@ -64,21 +66,23 @@ protected:
   typedef LA::MPI::Vector vector_t;
 
   /**
-   * Copy @p configure into #configure_qc member reconfigure QC without
-   * changing #atom_data.atoms.
-   * In doing so the association between the mesh and all the atoms is kept
-   * as is; energy atoms can be updated according to the new sampling rules
-   * and parameters given in @p configure.
+   * Copy @p configure into #configure_qc member to reconfigure QC without
+   * changing CellMoleculeData::cell_molecules of #cell_molecule_data.
+   * In doing so the initially set association between the mesh and all the
+   * atoms is kept as is; energy molecules should be updated according to the
+   * new sampling rules and/or certain parameters provided in @p configure.
    *
-   * Typically, parsing atoms and assigning cells to the atoms for a given
+   * Typically, parsing atom data and assigning cells to molecules for a given
    * large atomistic system is one of the most time consuming part of a large
    * QC simulation. For comparison of QC simulations with the same initially
    * given atomistic system and the same #triangulation but slightly altered
    * #configure_qc (say because of a slightly different
-   * ConfigureQC::cluster_radius or ConfigureQC::ghost_cell_layer_thickness),
-   * parsing atoms and assigning cells to the atoms again are redundant
-   * operations. Therefore, to avoided re-setting up of #atom_data.atoms
-   * the current QC object can be reconfiguring QC using this function.
+   * ConfigureQC::cluster_radius), parsing atoms and assigning cells to the
+   * atoms again are redundant operations (unless
+   * ConfigureQC::ghost_cell_layer_thickness is changed in which the cells
+   * and molecules association should be rebuilt). Therefore, to avoided
+   * re-setting up of CellMoleculeData::cell_molecules of #cell_molecule_data
+   * the current QC object can be reconfigured using this function.
    */
   void reconfigure_qc(const ConfigureQC &configure);
 
@@ -87,34 +91,34 @@ protected:
    */
   void setup_triangulation();
 
+  // TODO: rename it to setup_cell_molecules()?
   /**
-   * Setup few data members in #atom_data (namely: AtomData::atoms,
-   * AtomData::masses and AtomData::atomtype_to_atoms) of the current
-   * MPI process. However, this function doesn't update
-   * AtomData::energy_atoms in #atom_data, this is done by
+   * Setup few data members in #cell_molecule_data (namely:
+   * CellMoleculeData::cell_molecules,
+   * CellMoleculeData::masses and
+   * CellMoleculeData::charges) of the current MPI process. However, this
+   * function doesn't update CellMoleculeData::cell_energy_molecules of
+   * #cell_molecule_data, this is done by
    * QC::setup_energy_atoms_with_cluster_weights().
    *
-   * This function initializes the primary data member AtomData::atoms
-   * which holds an association between the locally relevant active cells of
-   * the underlying #triangulation and all the atoms in it. This is
-   * accomplished by calling AtomHandler::parse_atoms_and_assign_to_cells().
-   *
-   * @note The primary data member AtomData::atoms in #atom_data should be
-   * used only to initialize AtomData::energy_atoms in #atom_data. And
-   * AtomData::energy_atoms in #atom_data should be used to compute energy or
-   * force using the quasicontinuum approach.
+   * @note The primary data member CellMoleculeData::cell_molecules in
+   * #cell_molecule_data should be used only to initialize
+   * CellMoleculeData::cell_energy_molecules in #cell_molecule_data. And
+   * CellMoleculeData::cell_energy_molecules in #cell_molecule_data should be
+   * used to compute energy or force using the quasicontinuum approach.
    */
   void setup_atoms();
 
+  // TODO: rename it to setup_cell_energy_molecules()
   /**
-   * Setup AtomData::energy_atoms, with appropriate cluster weights,
-   * in #atom_data of the current MPI process.
+   * Setup CellMoleculeData::cell_energy_molecules of #cel_molecule_data of
+   * the current MPI process with appropriate cluster weights.
    *
-   * All the cluster atoms get a non zero cluster weight while the
-   * non-cluster atoms get a zero cluster weight. #configure_qc creates a
-   * shared pointer to the derived class of Cluster::WeightsByBase based on
-   * the chosen method (or sampling rule) to update cluster weights of
-   * cluster atoms.
+   * All the cluster molecules get a non-zero cluster weight while all the
+   * other energy (non-cluster) molecules get a zero cluster weight. Data
+   * member #configure_qc creates a shared pointer to a suitable derived
+   * class of Cluster::WeightsByBase based on the chosen method
+   * (or sampling rule) to update cluster weights of energy molecules.
    */
   void setup_energy_atoms_with_cluster_weights();
 
@@ -123,19 +127,21 @@ protected:
    */
   void setup_system ();
 
+  // TODO: rename it to update_positions()?
   /**
-   * Update positions of the energy atoms according to the given
+   * Update positions of the atoms of energy molecules
+   * (CellMoleculeData::cell_energy molecules) according to the given
    * @p locally_relevant_displacement of finite element displacement field.
    *
    * @note: During a typical minimization call in ROL, this function is called
    * after each iterate is formed. Internally in ROL, update() function of the
    * objective class is called after each iterate is formed and is responsible
    * for updating the data members of the objective class. Therefore,
-   * update_energy_atoms_positions() is separated from
-   * calculate_energy_gradient().
+   * the current function is separated from calculate_energy_gradient().
    */
   void update_energy_atoms_positions();
 
+  // TODO : rename to just compute()?
   /**
    * Return the computed energy of the atomistic system using QC approach, and
    * update the its @p gradient if @tparam ComputeGradient is set true.
@@ -147,7 +153,6 @@ protected:
   template<bool ComputeGradient=true>
   double calculate_energy_gradient (vector_t &gradient) const;
 
-  // TODO: implement the logic above. For now just use all atoms.
   /**
    * Given cells and dof handler, for each cell set-up FEValues object with
    * quadrature made of those atoms, which we are interested in. Namely
@@ -228,14 +233,8 @@ protected:
    * Auxiliary class with all the information needed per cell for
    * calculation of energy and forces in quasi-continuum method.
    *
-   * Since initial positions of atoms is generally random in each
+   * Since initial positions of molecules is generally random in each
    * element, we have to have a separate FEValues object for each cell.
-   *
-   * The most tricky part in non-local methods like molecular mechanics
-   * within the FE approach is to get the following association link:
-   *
-   * cell -> atom_id -> neighbour_id -> neighbour_cell -> local_neighbour_id
-   *
    */
   struct AssemblyData
   {
@@ -257,13 +256,13 @@ protected:
      * FEValues object to evaluate fields and shape function values at
      * quadrature points.
      *
-     * The @see energy_atoms data member of AtomData holds association between
-     * all the locally relevant active cells and the locally relevant energy
-     * atoms of the current MPI process. The positions of atoms' associated
-     * to a particular active cell are updated according to the (linearly
-     * varying) displacement field within the cell.
-     * The displacement within the cell can be obtained using FEValues object
-     * of the cell.
+     * The CellMoleculeData::cell_energy_molecules of #cell_molecule_data holds
+     * the association between all the locally relevant active cells and the
+     * locally relevant energy molecules on the current MPI process.
+     * The positions of molecules' associated to a particular active cell are
+     * updated according to the (linearly varying) displacement field within
+     * the cell. The displacement within the cell can be obtained using
+     * FEValues object of the cell.
      */
     std::shared_ptr<FEValues<dim>> fe_values;
 
@@ -274,11 +273,13 @@ protected:
     /**
      * A vector to store displacements evaluated at quadrature points.
      *
-     * The size of this vector is exactly equal to the number of energy_atoms
-     * on a per cell basis. The order of this list is the same order in which
-     * the energy atoms are stored in energy_atoms on a per cell basis.
+     * The size of this vector is exactly equal to the number of energy
+     * molecules on a per cell basis. The order of this list is the same order
+     * in which the energy molecules are stored in energy_atoms on a per cell
+     * basis.
      */
     mutable std::vector<Tensor<1,dim>> displacements;
+
   };
 
   /**
@@ -289,14 +290,14 @@ protected:
 
   /**
    * The primary atom data object that holds cell based atom data structures.
-   * Cell based atom data structures rely on the association between atoms
+   * Cell based atom data structures rely on the association between molecules
    * and mesh.
    */
   CellMoleculeData<dim> cell_molecule_data;
 
   /**
-   * AtomHandler object to manage the cell based atom data mainly through
-   * initializing or updating atom_data.
+   * MoleculeHandler object to manage the cell based neighbor lists of the
+   * system.
    */
   MoleculeHandler<dim> molecule_handler;
 
