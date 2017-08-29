942181b1bc2a40cfa9d78ea9067ea81e1f6b0cca
vishalkenchan <vishal.boddu@fau.de>
2017-08-29 16:56:24 +0200

    variables name change

--- source/core/qc.cc
+++ source/core/qc.cc
@@ -906,44 +906,44 @@ double QC<dim, PotentialType, atomicity>::compute_local (vector_t &gradient) con
           std::array<Tensor<1, dim>, atomicity>
           gradient_I;
 
-          Tensor<1, dim> molecular_IJ_i, intra_I_i; // Temporary variables.
+          Tensor<1, dim> molecular_IJ_atom_stamp, intra_I_atom_stamp; // Temporary variables.
           for (int atom_stamp = 0; atom_stamp < atomicity; ++atom_stamp)
             {
-              molecular_IJ_i = 0.;
-              intra_I_i      = 0.;
+              molecular_IJ_atom_stamp = 0.;
+              intra_I_atom_stamp      = 0.;
 
               // Summation over all j \in J (row sum of the Table).
               for (int j = 0; j < atomicity; ++j)
-                molecular_IJ_i += molecular_IJ.second(atom_stamp, j);
+                molecular_IJ_atom_stamp += molecular_IJ.second(atom_stamp, j);
 
               for (int j = 0; j < atomicity; ++j)
-                intra_I_i += intra_molecular_I.second(atom_stamp, j);
+                intra_I_atom_stamp += intra_molecular_I.second(atom_stamp, j);
 
               gradient_I[atom_stamp] =
-                molecular_IJ_i                    *  scale_energy +
-                (intra_I_i * 0.5               +
+                molecular_IJ_atom_stamp           * scale_energy +
+                (intra_I_atom_stamp * 0.5     +
                  external_I.second[atom_stamp]  ) * molecule_I.cluster_weight;
             }
 
           std::array<Tensor<1, dim>, atomicity>
           gradient_J;
 
-          Tensor<1, dim> molecular_IJ_j, intra_J_j; // Temporary variables.
+          Tensor<1, dim> molecular_IJ_atom_stamp, intra_J_atom_stamp; // Temporary variables.
           for (int atom_stamp = 0; atom_stamp < atomicity; ++atom_stamp)
             {
-              molecular_IJ_j = 0.;
-              intra_J_j      = 0.;
+              molecular_IJ_atom_stamp = 0.;
+              intra_J_atom_stamp      = 0.;
 
               // Summation over all j \in J (row sum of the Table).
               for (int i = 0; i < atomicity; ++i)
-                molecular_IJ_j += molecular_IJ.second(i, atom_stamp);
+                molecular_IJ_atom_stamp += molecular_IJ.second(i, atom_stamp);
 
               for (int i = 0; i < atomicity; ++i)
-                intra_J_j += intra_molecular_J.second(i, atom_stamp);
+                intra_J_atom_stamp += intra_molecular_J.second(i, atom_stamp);
 
               gradient_J[atom_stamp] =
-                -molecular_IJ_j                    *  scale_energy +
-                (intra_J_j * 0.5               +
+                -molecular_IJ_atom_stamp          *  scale_energy +
+                (intra_J_atom_stamp * 0.5     +
                  external_J.second[atom_stamp]  ) * molecule_J.cluster_weight;
             }
 
